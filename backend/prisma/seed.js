f10b38630e118cb1f5146d6985093a98ef429a92
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-02-10 11:59:40 -0500

    build: create prisma seed script to populate characters and coordinates
    install @prisma/adapter-pg and pg

---
+++ backend/prisma/seed.js
@@ -0,0 +1,72 @@
+// backend/prisma/seed.js
+
+const { PrismaClient } = require("../generated/prisma");
+const { Pool } = require("pg");
+const { PrismaPg } = require("@prisma/adapter-pg");
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const createMany = await prisma.character.createMany({
+    data: [
+      {
+        name: "Mulder",
+        x_coord: 39.285714285714285,
+        y_coord: 32.04651162790697,
+      },
+      {
+        name: "Meyers",
+        x_coord: 13.642857142857142,
+        y_coord: 39.25581395348837,
+      },
+      {
+        name: "Plissken",
+        x_coord: 21.357142857142858,
+        y_coord: 24.325581395348834,
+      },
+      {
+        name: "Headcrab",
+        x_coord: 9.428571428571429,
+        y_coord: 26.41860465116279,
+      },
+      {
+        name: "Freddy",
+        x_coord: 52,
+        y_coord: 13.627906976744185,
+      },
+      {
+        name: "Abe",
+        x_coord: 13.428571428571429,
+        y_coord: 46.6046511627907,
+      },
+      {
+        name: "Chucky",
+        x_coord: 23.714285714285715,
+        y_coord: 19.627906976744185,
+      },
+      {
+        name: "Gizmo",
+        x_coord: 97.78571428571429,
+        y_coord: 46.651162790697676,
+      },
+      {
+        name: "Samara",
+        x_coord: 90.07142857142857,
+        y_coord: 46.23255813953489,
+      },
+    ],
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
