ce7d578655776196f46709786ea92f5420b7d5f8
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2026-02-17 11:12:00 -0500

    feat: create scores.js for submitting user data to schema

--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -1,1 +1,21 @@
 // backend/routes/scores.js
+
+const express = require("express");
+const { prisma } = require("../lib/prisma");
+
+const router = express.Router();
+
+// submit user's name and time (score)
+router.post("/submit", async (req, res) => {
+  try {
+    // extract user's name and time
+    const { name, time } = req.body;
+
+    const userData = await prisma.score.create({ data: { name, time } });
+    return res.send(userData);
+  } catch (error) {
+    res.status(404).json({ error: "Not found" });
+  }
+});
+
+module.exports = router;
